e88bad21eaac00a4d00e747d8b657d8196eba653
dalipkumar703 <dalipkumar703@gmail.com>
2019-02-03 18:30:41 +0530

    Update blogsearch

--- blogs_spider.py
+++ blogs_spider.py
@@ -2,17 +2,18 @@ import scrapy
 
 class BlogsSpider(scrapy.Spider):
 	name="blogs"
+	inc = 1
 	start_urls = [
-		'http://knowledge.wharton.upenn.edu/?s=behaviour economics',
+		'http://knowledge.wharton.upenn.edu/?s=behavior economics',
 	]
 	
 	def parse(self, response):
 		reading = []
 		for page in response.css('a'):
-			if len(page.css('a::text').re(r'[a-zA-Z\s:,\']*.Behavioral\sEconomics.*')) > 0:
+			if len(page.css('a::text').re(r'[a-zA-Z\s:,\']*.Economic.*')) > 0:
 				reading.append(page.css('a::text'))
 		for read in reading:
-			print(read.get())
-	
-
-
+			print(read.get())	
+		BlogsSpider.inc = BlogsSpider.inc + 1
+		if BlogsSpider.inc < 20:
+			yield scrapy.Request(url='http://knowledge.wharton.upenn.edu/page/'+str(BlogsSpider.inc)+'/?s=behavior+economics',callback=self.parse)	
